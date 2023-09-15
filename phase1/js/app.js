7e5324988eabb8c6929ad8627ebc8678b1c5d2e7
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-09-14 18:56:39 -0600

    buttons added to list items and done list works
    the done list now works after clicking complete. and there are buttons for removing and completing

---
+++ phase1/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    // Define the click event handler for the "Add" button
+    $("#addButton").click(function() {
+        // Get the task input value
+        let taskInput = $("#newTask");
+        let task = taskInput.val();
+
+        // Create a new list item
+        let listItem = $("<li>").text(task);
+        // Create "Remove" and "Complete" buttons
+        let removeButton = $("<button>").text("Remove");
+        let completeButton = $("<button>").text("Complete");
+
+        // Append buttons to the list item
+        listItem.append(removeButton, completeButton);
+
+         // Define click event handler for the "Remove" button
+         removeButton.click(function() {
+            listItem.remove();
+        });
+
+        // Define click event handler for the "Complete" button
+        completeButton.click(function() {
+            let listlItemComplete = listItem.css("text-decoration", "line-through");
+            // Remove the "Complete" button
+            completeButton.remove();
+
+            $("#doneList").append(listlItemComplete);
+        });
+        
+        // Add the list item to the todoList
+        $("#todoList").append(listItem);
+
+        // Clear the task input
+        taskInput.val("");
+    });
+});
