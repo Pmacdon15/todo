62aa4dad877a1b577086b2b64b1d398a23467354
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-10-28 16:04:31 -0600

    completed and deleted fixed

--- phase2/client/js/app.js
+++ phase2/client/js/app.js
@@ -1,103 +1,96 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const taskList = document.getElementById("todoList");
-    const completedList = document.getElementById("completedList");
-    const form = document.getElementById("addTodoForm");
-  
-    loadTodos();
-    console.log("DOM fully loaded and parsed");
-  
-    // Functions below --------------------------------------------------
-  
-    function createListItem(todo, parentList) {
-      const li = document.createElement("li");
-      li.innerText = todo.title + " - " + todo.description;
-  
-      // Create a completed button if the todo is not completed
-      const completedButton = document.createElement("button");
-      completedButton.innerText = "Completed";
-      completedButton.addEventListener("click", () => {
-        completedTodo(todo.id);
-      });
-  
-      if (!todo.completed) {
-        li.appendChild(completedButton);
-      }
-  
-      // Create a delete button
-      const deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      deleteButton.addEventListener("click", () => {
-        deleteTodo(todo.id);
-      });
-  
-      // Append the delete button to the list item
-      li.appendChild(deleteButton);
-  
-      // Append the list item to the specified parent list
-      parentList.appendChild(li);
+  const taskList = document.getElementById("todoList");
+  const completedList = document.getElementById("completedList");
+  const form = document.getElementById("addTodoForm");
+
+  loadTodos();
+  console.log("DOM fully loaded and parsed");
+
+  // Functions below --------------------------------------------------
+
+  function createListItem(todo, parentList) {
+    const li = document.createElement("li");
+    li.innerText = todo.title + " - " + todo.description;
+
+    // Create a completed button if the todo is not completed
+    const completedButton = document.createElement("button");
+    completedButton.innerText = "Completed";
+    completedButton.addEventListener("click", () => {
+      completedTodo(todo.id);
+    });
+
+    if (!todo.completed) {
+      li.appendChild(completedButton);
     }
-  
-    function loadTodos() {
-      fetch("/todo")
-        .then((response) => response.json())
-        .then((data) => {
-          if (Array.isArray(data.todo)) {
-            data.todo.forEach((todo) => {
-              if (todo.completed) {
-                createListItem(todo, completedList);
-              } else {
-                createListItem(todo, taskList);
-              }
-            });
-          } else {
-            console.error("Data.todo is not an array:", data.todo);
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  
-    function completedTodo(todoId) {
-      fetch(`/todo/${todoId}`, {
-        method: "PUT",
+
+    // Create a delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      deleteTodo(todo.id);
+    });
+
+    // Append the delete button to the list item
+    li.appendChild(deleteButton);
+
+    // Append the list item to the specified parent list
+    parentList.appendChild(li);
+  }
+
+  function loadTodos() {
+    fetch("/todo")
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data.todo)) {
+          data.todo.forEach((todo) => {
+            if (todo.completed) {
+              createListItem(todo, completedList);
+            } else {
+              createListItem(todo, taskList);
+            }
+          });
+        } else {
+          console.error("Data.todo is not an array:", data.todo);
+        }
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          // You can refresh the lists after completing a todo if needed
-          // For example, you can clear the lists and re-fetch the data
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-  
-      // todo convert to jquery
-      taskList.innerHTML = "";
-      completedList.innerHTML = "";
-  
-      loadTodos();
-    }
-  
-    function deleteTodo(todoId) {
-      fetch(`/todo/${todoId}`, {
-        method: "DELETE",
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function completedTodo(todoId) {
+    fetch(`/todo/${todoId}`, {
+      method: "PUT",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // todo convert to jquery
+        taskList.innerHTML = "";
+        completedList.innerHTML = "";
+
+        loadTodos();
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          // You can refresh the lists after deleting a todo if needed
-          // For example, you can clear the lists and re-fetch the data
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-  
-      // todo convert to jquery
-      taskList.innerHTML = "";
-      completedList.innerHTML = "";
-  
-      loadTodos();
-    }
-  });
-  
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function deleteTodo(todoId) {
+    fetch(`/todo/${todoId}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // todo convert to jquery
+        taskList.innerHTML = "";
+        completedList.innerHTML = "";
+
+        loadTodos();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+});
